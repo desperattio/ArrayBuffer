a399cffef744d4a3c93ec9932902aa14de220c41
katherine <ek.gvozd@yandex.ru>
2024-06-02 19:28:56 +0300

    added class and tests

---
+++ src/__tests__/test.js
@@ -0,0 +1,15 @@
+import ArrayBufferConverter from "..";
+import { getBuffer } from "..";
+
+test('test loading buffer', () => {
+  const data = new ArrayBufferConverter();
+  data.load(getBuffer());
+  expect(data.buffer).toEqual(getBuffer());
+});
+
+test('test buffer to string', () => {
+  const data = new ArrayBufferConverter();
+  data.load(getBuffer());
+  const dataToStr = '[123,34,100,97,116,97,34,58,123,34,117,115,101,114,34,58,123,34,105,100,34,58,49,44,34,110,97,109,101,34,58,34,72,105,116,109,97,110,34,44,34,108,101,118,101,108,34,58,49,48,125,125,125]';
+  expect(data.toString()).toBe(dataToStr);
+})
